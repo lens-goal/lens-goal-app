7e74ab32720cebc2cb8bc714a5a2854cc6543921
Grzegorz Kubik <grzeznx2@gmail.com>
2023-03-04 11:09:07 -0700

    Create step 1 of goal creation form

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import React from "react";
-import styles from "../styles/Header.module.css";
 import SignInButton from "./SignInButton";
 
 export default function Header() {
--- src/pages/goals/new-goal.tsx
+++ src/pages/goals/new-goal.tsx
@@ -1,28 +1,61 @@
-import { Web3Button } from '@thirdweb-dev/react';
-import { useFormik } from 'formik';
+import { Web3Button } from "@thirdweb-dev/react";
+import { useFormik } from "formik";
+import Image from "next/image";
+import Button from "../../components/Button";
 
 export default function NewGoal() {
   const formik = useFormik({
-
     initialValues: {
-      description: '',
-      verificationCriteria: '',
-      preProof: '',
+      description: "",
+      verificationCriteria: "",
+      preProof: "",
       amount: 0,
       deadline: 0,
-      token: '0x294210dDbC38114dD6EE4959B797A0D2171f220b',
-      inEther: false
+      token: "0x294210dDbC38114dD6EE4959B797A0D2171f220b",
+      inEther: false,
     },
 
-    onSubmit: values => {
+    onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
-
   });
 
   return (
     <form onSubmit={formik.handleSubmit}>
-      <div>
+      <div className="flex w-100 container mx-auto px-8 pt-4">
+        <div className="flex grow">
+          <div className="w-1/2 flex justify-center">
+            <Image
+              width={600}
+              height={600}
+              src="/heroimageLensGoal.png"
+              alt=""
+            ></Image>
+          </div>
+          <div className="flex flex-col grow justify-center items-center">
+            <div className="mb-8">
+              <h2 className="text-center text-3xl">
+                What is your
+                <span className="block">GOAL?</span>
+              </h2>
+            </div>
+            <div className="self-stretch mb-8">
+              <textarea
+                id="description"
+                name="description"
+                onChange={formik.handleChange}
+                value={formik.values.description}
+                rows={5}
+                className="w-full px-4 py-2 text-gray-700 border rounded-lg drop-shadow-lg focus:outline-none focus:border-blue-500"
+              />
+            </div>
+            <div className="self-end">
+              <Button text="NEXT"></Button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div>
       <label htmlFor="description">Description</label>
       <input
         id="description"
@@ -31,78 +64,85 @@ export default function NewGoal() {
         onChange={formik.handleChange}
         value={formik.values.description}
         />
-        </div>
+        </div> */}
       <div>
-      <label htmlFor="verificationCriteria">Verification Criteria</label>
-      <input
-        id="verificationCriteria"
-        name="verificationCriteria"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.verificationCriteria}
+        <label htmlFor="verificationCriteria">Verification Criteria</label>
+        <input
+          id="verificationCriteria"
+          name="verificationCriteria"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.verificationCriteria}
         />
-        </div>
+      </div>
       <div>
-      <label htmlFor="preProof">Pre Proof</label>
-      <input
-        id="preProof"
-        name="preProof"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.preProof}
+        <label htmlFor="preProof">Pre Proof</label>
+        <input
+          id="preProof"
+          name="preProof"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.preProof}
         />
-        </div>
+      </div>
       <div>
-      <label htmlFor="amount">Amount</label>
-      <input
-        id="amount"
-        name="amount"
-        type="number"
-        onChange={formik.handleChange}
-        value={formik.values.amount}
+        <label htmlFor="amount">Amount</label>
+        <input
+          id="amount"
+          name="amount"
+          type="number"
+          onChange={formik.handleChange}
+          value={formik.values.amount}
         />
-        </div>
+      </div>
       <div>
-      <label htmlFor="deadline">Deadline</label>
-      <input
-        id="deadline"
-        name="deadline"
-        type="number"
-        onChange={formik.handleChange}
-        value={formik.values.deadline}
+        <label htmlFor="deadline">Deadline</label>
+        <input
+          id="deadline"
+          name="deadline"
+          type="number"
+          onChange={formik.handleChange}
+          value={formik.values.deadline}
         />
-        </div>
+      </div>
       <div>
-      <label htmlFor="token">Token</label>
-      <input
-        id="token"
-        name="token"
-        type="string"
-        onChange={formik.handleChange}
-        value={formik.values.token}
+        <label htmlFor="token">Token</label>
+        <input
+          id="token"
+          name="token"
+          type="string"
+          onChange={formik.handleChange}
+          value={formik.values.token}
         />
-        </div>
+      </div>
       <div>
-      <label htmlFor="inEther">In Ether</label>
-      <input
-        id="inEther"
-        name="inEther"
-        type="checkbox"
-        onChange={formik.handleChange}
-        checked={formik.values.inEther}
+        <label htmlFor="inEther">In Ether</label>
+        <input
+          id="inEther"
+          name="inEther"
+          type="checkbox"
+          onChange={formik.handleChange}
+          checked={formik.values.inEther}
         />
-        </div>
- 
-    <Web3Button
-      contractAddress="0x3A3aB8A753d8E490b02941d7Dc86C04Aa392E239"
-      action={(contract) => {
-        contract.call("makeNewGoal", formik.values.description, formik.values.verificationCriteria, formik.values.inEther, formik.values.amount, formik.values.token, formik.values.deadline, formik.values.deadline)
-      }}
-    >
-      makeNewGoal
-    </Web3Button>
-  
+      </div>
 
+      <Web3Button
+        contractAddress="0x3A3aB8A753d8E490b02941d7Dc86C04Aa392E239"
+        action={(contract) => {
+          contract.call(
+            "makeNewGoal",
+            formik.values.description,
+            formik.values.verificationCriteria,
+            formik.values.inEther,
+            formik.values.amount,
+            formik.values.token,
+            formik.values.deadline,
+            formik.values.deadline
+          );
+        }}
+      >
+        makeNewGoal
+      </Web3Button>
     </form>
   );
-  }
+}
