c30c69b0704c8106e03a1b4d179ceabfad1bb929
Grzegorz Kubik <grzeznx2@gmail.com>
2023-03-04 09:53:11 -0700

    Style header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-import { Web3Button } from "@thirdweb-dev/react";
 import Link from "next/link";
 import React from "react";
 import styles from "../styles/Header.module.css";
@@ -7,19 +6,17 @@ import SignInButton from "./SignInButton";
 export default function Header() {
   return (
     <>
-      <div className={styles.headerContainer}>
-        <div className={styles.left}>
+      <div className="flex container mx-auto px-8 pt-4">
+        <div className="flex grow items-center gap-10">
           <Link href="/goals/new-goal">My Goals</Link>
           <Link href="/goals/friends-goals">Frens' Goals</Link>
           <Link href="/goals/group-goals">Group Goals</Link>
           <Link href="/Evidence">Evidence</Link>
         </div>
-
-        <div className={styles.right}>
+        <div>
           <SignInButton />
         </div>
       </div>
-      <div style={{ height: 64 }} />
     </>
   );
 }
