88c6377c17dece8620def8c6d8111391e1e10906
Grzegorz Kubik <grzeznx2@gmail.com>
2023-03-04 01:39:26 -0700

    Create hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import FeedPost from "../components/FeedPost";
 import {
   PublicationMainFocus,
@@ -33,10 +35,29 @@ export default function Home() {
   return (
     <div className={styles.container}>
       {/* Iterate over the array of items inside the data field  */}
-      <div className={styles.postsContainer}>
-        {data?.explorePublications.items.map((publication) => (
-          <FeedPost publication={publication} key={publication.id} />
-        ))}
+      <div>
+        <div className="flex align-middle">
+          <div className="w-1/2">
+            <Image width={650} height={650} src='/heroimageLensGoal.png' alt=''></Image>  
+          </div>
+          <div className="flex flex-col justify-center w-1/2 text-center">
+            <h1 className="text-6xl mb-4">
+              Put your money
+              <br></br>
+              where your goal is!
+            </h1>
+            <span className="text-lg">Socially accountable goals, powered by your frens</span>
+            <div className="mt-8">
+              <Link href="/goals/new-goal" className="bg-emerald-900 px-20 py-4 text-white rounded-lg">
+                Set your goal
+              </Link>
+            </div>
+          </div>
+
+        </div>
+        <div>
+          <Image width={1850} height={2953} src='/dottedLine.svg' alt=''></Image> 
+        </div>
       </div>
     </div>
   );
